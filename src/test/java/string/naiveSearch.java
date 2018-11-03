b30f43e703c889032950009d389dfd5915d16288
mongmongi <ldh793692@gmail.com>
2018-11-03 18:10:32 +0900

    naive search

---
+++ src/test/java/string/naiveSearch.java
@@ -0,0 +1,38 @@
+package string;
+
+
+import org.hamcrest.core.Is;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertThat;
+
+public class naiveSearch {
+
+    private String Haystack = "adfdsasafaefadfaef";
+    private String Search = "faef";
+
+    @Test
+    public void test(){
+        List<Integer> locationMatched = new ArrayList<Integer>();
+        search(Haystack, Search, locationMatched);
+        assertThat(locationMatched.get(0), Is.is(8));
+    }
+
+    public static void search(String haystack, String search, List<Integer> locationMatched){
+        for(int i = 0; i + search.length() < haystack.length(); i++){
+            boolean matched = true;
+            for(int j = 0; j < search.length(); j++){
+                if(haystack.charAt(i + j) != search.charAt(j)){
+                    matched = false;
+                    break;
+                }
+            }
+            if(matched) {
+                locationMatched.add(i);
+            }
+        }
+    }
+}
